eb41fa784a0f201d42fcd537788e158d0c377e06
Максим <molir10@mail.ru>
2021-07-04 21:12:52 +0300

    todo app

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="todo-list">
+    <input
+      v-model="allChecked"
+      type="checkbox"
+      @input="toggleAllChecked($event.target.checked)"
+    >
+    <todo-list
+      :title="currentTab"
+      :list="currentList"
+      @removeItem="removeItem"
+    />
+    <form @submit.prevent="addNewTodo">
+      <input
+        v-model="newTodo"
+        type="text"
+      >
+      <button type="submit">
+        Добавить
+      </button>
+    </form>
+    <p>{{ todosLeft }} items left</p>
+    <button
+      v-for="it in tabs"
+      :key="it"
+      class="mr-2"
+      :class="currentTab === it && 'active'"
+      @click="setCurrentTab(it)"
+    >
+      {{ it }}
+    </button>
+    <p>
+      <button @click="clearCompleted">
+        Clear completed
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+import uniqid from 'uniqid';
+import TodoList from './TodoList.vue';
+
+export default {
+  name: 'TodoApp',
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      tabs: ['All', 'Active', 'Completed'],
+      currentTab: 'All',
+      newTodo: '',
+      todoList: [{
+        id: uniqid(),
+        text: 'Купить молоко',
+        checked: true,
+      }, {
+        id: uniqid(),
+        text: 'Нассать на кота',
+        checked: false,
+      }],
+      allChecked: false,
+    };
+  },
+  computed: {
+    todosLeft() {
+      return this.todoList.filter((it) => !it.checked).length;
+    },
+    todoListActive() {
+      return this.todoList.filter((it) => !it.checked);
+    },
+    todoListCompleted() {
+      return this.todoList.filter((it) => it.checked);
+    },
+    currentList() {
+      const tabs = {
+        All: this.todoList,
+        Active: this.todoListActive,
+        Completed: this.todoListCompleted,
+      };
+      return tabs[this.currentTab];
+    },
+  },
+  watch: {
+    todoList: {
+      handler(val) {
+        if (this.settingNewItem) {
+          return;
+        }
+        this.allChecked = val.every((it) => it.checked);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    addNewTodo() {
+      const newTodoObj = {
+        id: uniqid(),
+        text: this.newTodo,
+        checked: false,
+      };
+      this.todoList.push(newTodoObj);
+      this.newTodo = '';
+    },
+    removeItem(id) {
+      this.todoList = this.todoList.filter((it) => it.id !== id);
+    },
+    clearCompleted() {
+      this.todoList = this.todoList.filter((it) => !it.checked);
+    },
+    toggleAllChecked(val) {
+      this.todoList.forEach((it) => {
+        it.checked = val;
+      });
+    },
+    setCurrentTab(val) {
+      this.currentTab = val;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.mr-2 {
+  margin-right: 10px;
+}
+
+button.active {
+  background-color: red;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ title }}
+    <div
+      v-for="it in list"
+      :key="it.id"
+      class="todo-list__item"
+    >
+      <input
+        v-model="it.checked"
+        type="checkbox"
+      >
+      {{ it.text }}
+      <i @click="$emit('removeItem', it.id)">X</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    title: String,
+    list: Array,
+  },
+};
+</script>
