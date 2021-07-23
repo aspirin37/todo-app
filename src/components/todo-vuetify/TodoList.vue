57d8311bd39191479f96912d51e39b25753cbf50
Mr.Pupochek <mr.pupochek@192-168-0-103.local>
2021-07-23 15:50:00 +0300

    add form

---
+++ src/components/todo-vuetify/TodoList.vue
@@ -0,0 +1,40 @@
+!<template>
+  <div>
+    <div
+      v-for="it in list"
+      :key="it.id"
+    >
+      <v-row class="pa-4 pb-2 row">
+        <v-checkbox
+          v-model="it.checked"
+        />
+        <v-text-field
+          class="no-bottom-line"
+          :value="it.text"
+        />
+        <button
+          @click="$emit('removeItem', it.id)"
+        >
+          <v-icon color="red">
+            mdi-close
+          </v-icon>
+        </button>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    list: Array,
+  },
+};
+</script>
+
+<style>
+.row {
+  border-bottom: 1px solid rgb(130, 130, 130);
+}
+</style>
